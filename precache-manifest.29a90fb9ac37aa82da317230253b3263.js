3a85a4a2ec5d3bf0fcb9818e6b1d1a3fa3e753b1
MOSES <MOSESLAZARO2019@outlook.com>
2019-11-10 19:00:29 +0300

    Updates

---
+++ precache-manifest.29a90fb9ac37aa82da317230253b3263.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3071bd84fc160c10d9d4dc1acb8a966a",
+    "url": "/Multiple-items-Carousel/index.html"
+  },
+  {
+    "revision": "97ab9cb76df99f972261",
+    "url": "/Multiple-items-Carousel/static/css/2.444d2bb3.chunk.css"
+  },
+  {
+    "revision": "a57241501dd742d84b90",
+    "url": "/Multiple-items-Carousel/static/css/main.33102095.chunk.css"
+  },
+  {
+    "revision": "97ab9cb76df99f972261",
+    "url": "/Multiple-items-Carousel/static/js/2.49eddfb9.chunk.js"
+  },
+  {
+    "revision": "a57241501dd742d84b90",
+    "url": "/Multiple-items-Carousel/static/js/main.6f236b47.chunk.js"
+  },
+  {
+    "revision": "a47b1aa8b94c76d2c952",
+    "url": "/Multiple-items-Carousel/static/js/runtime-main.ffd4103a.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/Multiple-items-Carousel/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/Multiple-items-Carousel/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/Multiple-items-Carousel/static/media/revicons.2feb69cc.eot"
+  }
+]);
